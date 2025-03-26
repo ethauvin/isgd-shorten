ca817dbafd7c75994b676001c9fbeb3c59f59aa5
Erik C. Thauvin <erik@thauvin.net>
2025-03-26 11:07:00 -0700

    Cleaned up compile options

--- src/bld/java/net/thauvin/erik/IsgdShortenBuild.java
+++ src/bld/java/net/thauvin/erik/IsgdShortenBuild.java
@@ -119,10 +119,9 @@ public class IsgdShortenBuild extends Project {
     @BuildCommand(summary = "Compiles the Kotlin project")
     @Override
     public void compile() throws Exception {
-        var options = new CompileOptions().jvmOptions("--enable-native-access=ALL-UNNAMED");
         new CompileKotlinOperation()
                 .fromProject(this)
-                .compileOptions(options)
+                .compileOptions(new CompileOptions().verbose(true))
                 .execute();
     }
 
