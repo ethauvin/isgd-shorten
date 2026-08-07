fc8b6096fecd2b9e941b693703c180661825cf7f
Erik C. Thauvin <erik@thauvin.net>
2026-08-06 20:32:49 -0700

    Bump Kotlin to version 2.4.10

--- src/bld/java/net/thauvin/erik/IsgdShortenBuild.java
+++ src/bld/java/net/thauvin/erik/IsgdShortenBuild.java
@@ -71,7 +71,7 @@ public class IsgdShortenBuild extends Project {
 
         repositories = List.of(MAVEN_LOCAL, MAVEN_CENTRAL);
 
-        final var kotlin = version(2, 3, 10);
+        final var kotlin = version(2, 4, 10);
         var junit = version(6, 0, 3);
         scope(compile)
                 .include(dependency("org.jetbrains.kotlin", "kotlin-stdlib", kotlin))
